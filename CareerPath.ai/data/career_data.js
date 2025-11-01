67cc348339e098e626763bf85712a675e908c9de
Kelsier <realkelsier@gmail.com>
2025-11-01 12:30:23 +0500

    Create career_data.js

---
+++ CareerPath.ai/data/career_data.js
@@ -0,0 +1,4 @@
+export const careers = [
+  { name: "Software Engineer", requiredSkills: ["Programming"], avgSalaryUSD: 85000, roadmap: ["Learn", "Build", "Apply"] },
+  { name: "Doctor", requiredSkills: ["Biology"], avgSalaryUSD: 120000, roadmap: ["School", "Residency"] }
+];
